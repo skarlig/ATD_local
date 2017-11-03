8306e675f53e4c15ce3f57c33c164c066d051e31
Chris Stefani <chris.stefani@outlook.com>
2017-11-03 09:46:35 +0100

    Remove driver factor to reduce complexity

--- atd_example/src/test/java/example/Tests.java
+++ atd_example/src/test/java/example/Tests.java
@@ -3,6 +3,8 @@ package example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -13,11 +15,13 @@ import java.util.concurrent.TimeUnit;
 
 public class Tests {
 
+    public WebDriver driver;
+
     public static final String cookieBannerButton = ".cc-btn.cc_btn_accept_all";
 
     @BeforeMethod
     public void setup() throws Exception {
-        WebDriver driver = DriverFactory.getInstance().getDriver();
+        createBrowserDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().fullscreen();
         driver.get("https://en.dawanda.com");
@@ -30,7 +34,6 @@ public class Tests {
 
     @Test
     public void loginDaWanda() {
-        WebDriver driver = DriverFactory.getInstance().getDriver();
         driver.findElement(By.className("header-user-welcome")).click();
         driver.findElement(By.id("username")).sendKeys("agileTestingDays");
         driver.findElement(By.id("login_credentials_password")).sendKeys("atd!rocks");
@@ -40,18 +43,64 @@ public class Tests {
 
     @AfterMethod
     public void closeDriver() {
-        DriverFactory.getInstance().removeDriver();
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+
+    private void createBrowserDriver() {
+        String os = detectOS();
+
+        String[] location = Tests.class.getProtectionDomain().getCodeSource().getLocation().toString().
+                split("build");
+
+        String driverPath = location[0].replace("file:", "").concat("/drivers/");
+
+        switch (os) {
+            case "windows":
+                System.setProperty("webdriver.gecko.driver", driverPath + "geckodriver_windows.exe");
+                System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver_windows.exe");
+                break;
+
+            case "linux":
+                System.setProperty("webdriver.gecko.driver", driverPath + "geckodriver_linux");
+                System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver_linux");
+                break;
+
+            case "mac":
+                System.setProperty("webdriver.gecko.driver", driverPath + "geckodriver_mac");
+                System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver_mac");
+                break;
+        }
+        if (System.getenv("BROWSER").contains("firefox")) {
+            driver = new FirefoxDriver();
+        } else {
+            driver = new ChromeDriver();
+        }
+    }
+
+    private String detectOS() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")) {
+            return "windows";
+        } else if (os.contains("nux") || os.contains("nix")) {
+            return "linux";
+        } else if (os.contains("mac")) {
+            return "mac";
+        } else {
+            System.out.println("Not supported OS");
+            return "other";
+        }
     }
 
     private void removeCookieBanner() throws Exception {
-        WebDriver driver = DriverFactory.getInstance().getDriver();
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("document.querySelector('" + cookieBannerButton + "').click();");
         waitUntilOverlayDisappears(cookieBannerButton);
     }
 
     private void waitUntilOverlayDisappears(String selector) throws Exception {
-        WebDriver driver = DriverFactory.getInstance().getDriver();
         WebDriverWait wait = new WebDriverWait(driver, 5);
         wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(selector)));
     }
